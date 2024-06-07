cb108f24f4d6d44b6f2c20faa2aada9b9d6d31b2
hsalamat <2hooman.salamat@gmail.com>
2024-06-07 10:35:31 -0400

    Adds extra validation that the provided id has the required format

--- ASPNETCORE8Tutorial/TestASPNETCORE/Program.cs
+++ ASPNETCORE8Tutorial/TestASPNETCORE/Program.cs
@@ -1,48 +1,22 @@
 ﻿using System.Collections.Concurrent;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddProblemDetails(); // Add the IProblemDetailsService
-
 WebApplication app = builder.Build();
-
-// Configure exception and error handlers
-// Normally you would only enable this in Production
-// But for demonstration purposes this is enabled in all environments
-// if(!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler();
-}
-app.UseStatusCodePages();
 
 var _fruit = new ConcurrentDictionary<string, Fruit>();
 
-app.MapGet("/", void () => throw new Exception("Demonstrating automatic ProblemDetails"));
-
-app.MapGet("/fruit", () => _fruit);
-
 app.MapGet("/fruit/{id}", (string id) =>
-    _fruit.TryGetValue(id, out var fruit)
-        ? TypedResults.Ok(fruit)
-        : Results.NotFound()); // standard error converted to ProblemDetails
-
-app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
-    _fruit.TryAdd(id, fruit)
-        ? TypedResults.Created($"/fruit/{id}", fruit)
-        : Results.ValidationProblem(new Dictionary<string, string[]>
-          {
-              { "id", new[] { "A fruit with this id already exists" } }
-        }));
-
-app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
 {
-    _fruit[id] = fruit;
-    return Results.NoContent();
-});
+    if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))     
+    {
+        return Results.ValidationProblem(new Dictionary<string, string[]>
+        {
+            {"id", new[] {"Invalid format. Id must start with 'f'"}}
+        });
+    }
 
-app.MapDelete("/fruit/{id}", (string id) =>
-{
-    _fruit.TryRemove(id, out _);
-    return Results.NoContent();
+    return _fruit.TryGetValue(id, out var fruit)
+            ? TypedResults.Ok(fruit)
+            : Results.Problem(statusCode: 404);
 });
 
 app.Run();
